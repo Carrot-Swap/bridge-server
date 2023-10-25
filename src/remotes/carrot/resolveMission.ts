b0ed7473a55ea16afdde2f73f7d9e7b2b871480d
boxfox619 <boxfoxsg619@gmail.com>
2023-10-25 18:54:42 +0900

    fix

--- src/remotes/carrot/resolveMission.ts
+++ src/remotes/carrot/resolveMission.ts
@@ -2,9 +2,12 @@ import { MISSION_KEY } from "constants/env";
 import { carrotRequester } from "./requester";
 
 export async function resolveMission(list: string[]) {
-  const res = await carrotRequester.post<boolean>(`/transaction/bridge`, {
-    list,
-    key: MISSION_KEY(),
-  });
+  const res = await carrotRequester.post<boolean>(
+    `/transaction/bridge/confirm`,
+    {
+      list,
+      key: MISSION_KEY(),
+    }
+  );
   return res.data;
 }
