038adf876640d285c2ab534c8c5d1b943214b1ea
boxfox619 <boxfoxsg619@gmail.com>
2024-10-16 16:27:51 +0900

    Fix webhook

--- src/remotes/slack.ts
+++ src/remotes/slack.ts
@@ -1,12 +1,15 @@
 import axios from "axios";
 
-export async function sendSlackNotify(message: string, mention?: boolean) {
-  await axios.post(
-    "https://hooks.slack.com/services/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",
+export async function sendSlackNotify(payload: string, mention?: boolean) {
+  const data =
+    typeof payload === "string"
+      ? { content: `${payload}${mention ? ` <@1137655771048443934>` : ""}` }
+      : payload;
+
+  axios.post(
+    "https://discord.com/api/webhooks/1296010580909363200/CwFvAsMaoi5dVWiss8x0CXW4HegfUcaFHoLc6-lBu1Zr5XcYr5FsBorLGOFNHL8tDrj2",
     {
-      text: `${message}${mention ? ` <@boxfoxsg619>` : ""}`,
-      username: "Carrot",
-      icon_emoji: ":carrot:",
+      data,
     }
   );
 }
