65e13160251faf0aa72035b446e8ed3457743e65
boxfox619 <boxfoxsg619@gmail.com>
2024-10-23 16:25:58 +0900

    Fix

--- src/worker/queue/checkAndSendGas.ts
+++ src/worker/queue/checkAndSendGas.ts
@@ -12,6 +12,8 @@ export interface ChargeGas {
   chainId: number;
 }
 
+const excludes: string[] = [];
+
 export async function checkAndSendGas(list: ChargeGas[]) {
   const { signUrls } = NETWORKS[ChainId.NEOX];
   const signer = getSigner(signUrls, ChainId.NEOX);
@@ -20,6 +22,9 @@ export async function checkAndSendGas(list: ChargeGas[]) {
   for (const item of list) {
     if (!NETWORKS[item.chainId]) {
       continue;
+    }
+    if (excludes.includes(item.txHash)) {
+      return;
     }
     try {
       sendSlackNotify(`try charge gas : ${JSON.stringify(item)}`);
@@ -31,27 +36,35 @@ export async function checkAndSendGas(list: ChargeGas[]) {
         receipt.to.toLowerCase() !==
           "0x31285fa517f7503f5b0fee341019f1d701353d0f"
       ) {
+        excludes.push(item.txHash);
+        sendSlackNotify("reject 1", true);
         continue;
       }
       const tx = await provider.getTransaction(item.txHash);
       const eth = Number(formatEther(tx.value));
       if (eth == 0) {
+        excludes.push(item.txHash);
+        sendSlackNotify("reject 2", true);
         continue;
       }
       const price = prices[SYMBOL_BY_CHAIN_ID[item.chainId]];
       const gasPrice = prices["GAS"];
       if (!price || !gasPrice) {
+        sendSlackNotify("reject 3", true);
+        excludes.push(item.txHash);
         continue;
       }
       const payAmountInUSD = eth * price;
       if (!payAmountInUSD || payAmountInUSD < 0.5 || payAmountInUSD > 100) {
-        sendSlackNotify("reject 4");
+        sendSlackNotify("reject 4", true);
+        excludes.push(item.txHash);
         continue;
       }
       const gasAmount = formatDecimals(payAmountInUSD / (gasPrice * 1.05), 9);
       const balance = await signer.provider.getBalance(signer.address);
       if (Number(formatEther(balance)) - gasAmount < 2) {
-        sendSlackNotify("reject 5");
+        sendSlackNotify("reject 5", true);
+        excludes.push(item.txHash);
         continue;
       }
       sendSlackNotify(`sending ${gasAmount} $GAS`);
